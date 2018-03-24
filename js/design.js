44fec79a33d35597947cec33ca21dd7688a7ba32
andrewnessin <andrewnesssinjim@yahoo.com>
2018-03-24 15:44:59 +0530

    feat: Animate the colors of the turned on header cells

--- js/design.js
+++ js/design.js
@@ -4,6 +4,15 @@ $(function() {
     $('#banner').append(new MatrixString("PIXEL ART MAKER").getHTMLString());
 
     $('.banner-matrix-cell-on').mouseenter(function(event){
-        $(event.target).css("background-color",headerColors[Math.floor(Math.random()*5)]);
+        $(event.target).animate({
+            opacity : 0
+        },
+        500,
+        function() {
+            $(event.target).css("background-color",headerColors[Math.floor(Math.random()*5)]);
+            $(event.target).animate({
+                opacity : 1
+            }, 500)
+        });
     })
 })
