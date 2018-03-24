ba6974987836ffe015b9cc8e8c81cea5de6dd18b
andrewnessin <andrewnesssinjim@yahoo.com>
2018-03-24 12:26:31 +0530

    feat: Color turned on header cells randomly when mouse is hovered

--- js/design.js
+++ js/design.js
@@ -1,3 +1,9 @@
+const headerColors = ["red","yellow","green","blue","violet","pink"];
+
 $(function() {
     $('#banner').append(new MatrixString("PIXEL ART MAKER").getHTMLString());
+
+    $('.banner-matrix-cell-on').mouseenter(function(event){
+        $(event.target).css("background-color",headerColors[Math.floor(Math.random()*5)]);
+    })
 })
